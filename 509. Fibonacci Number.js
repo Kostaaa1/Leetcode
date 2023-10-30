80b9a15fd4d74458b679c3f5a4c137e2f3c24cd2
Kostaaa1 <kostaarsic123@gmail.com>
2023-10-30 19:30:22 +0100

    v

--- 509. Fibonacci Number.js
+++ 509. Fibonacci Number.js
@@ -2,7 +2,7 @@
 
 // F(0) = 0, F(1) = 1
 // F(n) = F(n - 1) + F(n - 2), for n > 1.
-// Given n, calculate F(n).
+// Given n, calculate F(n)`
 
 // Example 1:
 
@@ -30,7 +30,3 @@ var fib = function (n) {
   if (n <= 1) return n;
   return fib(n - 2) + fib(n - 1);
 };
-
-console.log(fib(2));
-console.log(fib(3));
-console.log(fib(4));
