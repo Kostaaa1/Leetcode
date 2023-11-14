dca7b9009cac7572c3289b772411c2449b0d4783
Kostaaa1 <kostaarsic123@gmail.com>
2023-11-14 23:58:53 +0100

    v

---
+++ 35. Search Insert Position.js
@@ -0,0 +1,40 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+// Solution:
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (target === nums[i] || target < nums[i]) {
+      return i;
+    }
+
+    if (i === nums.length - 1) {
+      return i + 1;
+    }
+  }
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6], 7));
+console.log(searchInsert([1, 3, 5, 6], 0));
